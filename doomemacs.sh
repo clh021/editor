e9021dc2eb73d4b7f6334dae6fc6cb1380550751
chenlianghong <clh021@gmail.com>
2022-09-19 21:46:05 +0800

    updat doomemacs.sh

--- doomemacs.sh
+++ doomemacs.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 # leehom Chen clh021@gmail.com
+projUrl=https://github.com/doomemacs/doomemacs.git
 set -e
 cd "$( dirname "${BASH_SOURCE[0]}" )"
-currentPath=`pwd`
-conf=$currentPath/spacemacs/.emacs.d/init.el
-home=$currentPath/spacemacs/
+currentPath="$(pwd)"
+filenameExt="$(basename "$0")"
+filename="${filenameExt%%.*}"
+conf=$currentPath/$filename/.emacs.d/.dir-locals.el
+home=$currentPath/$filename/
 
 if ! test -f "$conf"; then
-	mkdir -p $( dirname $conf )
-	git clone --depth=1 https://github.com/syl20bnr/spacemacs.git spacemacs/.emacs.d
+	mkdir -p "$( dirname "$conf" )"
+	git clone --depth=1 $projUrl "$(dirname "$conf")"
+	HOME=$home ./doomemacs/.emacs.d/bin/doom install
 fi
 
 HOME=$home emacs
