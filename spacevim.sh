3d88d481f3e3d2d61f9b5bc6df70c5c2e7222c3a
chenlianghong <clh021@gmail.com>
2022-09-20 14:22:21 +0800

    add spacevim.sh

--- spacevim.sh
+++ spacevim.sh
@@ -1,17 +1,36 @@
 #!/usr/bin/env bash
 # leehom Chen clh021@gmail.com
-projUrl=https://github.com/syl20bnr/spacemacs.git
+# depends:
+#   - vim/neovim +python3
+#   - git
+#   - curl
+#
 set -e
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 currentPath="$(pwd)"
 filenameExt="$(basename "$0")"
 filename="${filenameExt%%.*}"
-conf=$currentPath/$filename/.emacs.d/init.el
-home=$currentPath/$filename/
+conf=$currentPath/$filename/.SpaceVim/init.el
+export HOME=$currentPath/$filename/
 
 if ! test -f "$conf"; then
-	mkdir -p "$( dirname "$conf" )"
-	git clone --depth=1 $projUrl "$(dirname "$conf")"
+	curl -sLf https://spacevim.org/install.sh | bash
 fi
 
-HOME=$home emacs
+# 优先启用 neovim, 其次是 vim
+VimBin=
+if ! command -v nvim &> /dev/null
+then
+  if ! command -v vim &> /dev/null
+  then
+    echo "Not found neovim and vim now..."
+    exit 1
+  else
+    echo "Not found neovim, use vim now..."
+    VimBin=$(which vim)
+  fi
+else
+  VimBin=$(which nvim)
+fi
+
+$VimBin
