e6eec32a2261d69664696a3d2860d97678fd6f4b
chenlianghong <clh021@gmail.com>
2022-09-21 12:15:16 +0800

    update lunarvim.sh

--- lunarvim.sh
+++ lunarvim.sh
@@ -10,7 +10,7 @@ cd "$( dirname "${BASH_SOURCE[0]}" )"
 currentPath="$(pwd)"
 filenameExt="$(basename "$0")"
 filename="${filenameExt%.*}"
-conf=$currentPath/$filename/.SpaceVim/init.vim
+conf=$currentPath/$filename/.local/bin/lvim
 export HOME=$currentPath/$filename/
 
 if ! test -f "$conf"; then
@@ -18,22 +18,7 @@ if ! test -f "$conf"; then
   # curl -sLf https://raw.fastgit.org/LunarVim/LunarVim/master/utils/installer/install.sh | bash
   bash <(curl -s https://raw.fastgit.org/lunarvim/lunarvim/master/utils/installer/install.sh)
 fi
-exit
 
 # 优先启用 neovim, 其次是 vim
-VimBin=
-if ! command -v nvim &> /dev/null
-then
-  if ! command -v vim &> /dev/null
-  then
-    echo "Not found neovim and vim now..."
-    exit 1
-  else
-    echo "Not found neovim, use vim now..."
-    VimBin=$(which vim)
-  fi
-else
-  VimBin=$(which nvim)
-fi
-
-$VimBin
+export PATH=$HOME/.local/bin:$PATH
+lvim
