577c93e936d59b9861e73fa6035bc6572f65b983
chenlianghong <clh021@gmail.com>
2022-09-21 12:27:59 +0800

    add nvchad.sh

---
+++ nvchad.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# leehom Chen clh021@gmail.com
+projUrl=https://github.com/NvChad/NvChad
+set -e
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+currentPath="$(pwd)"
+filenameExt="$(basename "$0")"
+filename="${filenameExt%.*}"
+conf=$currentPath/$filename/.config/nvim/init.lua
+home=$currentPath/$filename/
+
+if ! test -f "$conf"; then
+	mkdir -p "$( dirname "$conf" )"
+	git clone --depth=1 $projUrl "$(dirname "$conf")"
+fi
+
+HOME=$home nvim +PackerSync
